eb8e2cf1ce5569bccfb54f98d0fc2346c735e705
Huidae Cho <grass4u@gmail.com>
2021-10-23 03:22:59 -0400

    proj: Update to 8.1.1

--- src/proj.mk
+++ src/proj.mk
@@ -3,15 +3,15 @@
 PKG             := proj
 $(PKG)_WEBSITE  := https://trac.osgeo.org/proj/
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 5.1.0
-$(PKG)_CHECKSUM := 6b1379a53317d9b5b8c723c1dc7bf2e3a8eb22ceb46b8807a1ce48ef65685bb3
+$(PKG)_VERSION  := 8.1.1
+$(PKG)_CHECKSUM := 82f1345e5fa530c407cb1fc0752e83f8d08d2b98772941bbdc7820241f7fada2
 $(PKG)_SUBDIR   := proj-$($(PKG)_VERSION)
 $(PKG)_FILE     := proj-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://download.osgeo.org/proj/$($(PKG)_FILE)
 $(PKG)_DEPS     := cc
 
 define $(PKG)_UPDATE
-    $(WGET) -q -O- 'https://proj4.org/download.html' | \
+    $(WGET) -q -O- 'https://proj.org/download.html' | \
     $(SED) -n 's,.*proj-\([0-9][^>]*\)\.tar.*,\1,p' | \
     head -1
 endef
@@ -21,8 +21,6 @@ define $(PKG)_BUILD
         $(MXE_CONFIGURE_OPTS) \
         --with-mutex
     $(MAKE) -C '$(1)' -j '$(JOBS)'
-    # remove header which is not installed since 4.8.0
-    rm -f '$(PREFIX)/$(TARGET)'/include/projects.h
     $(MAKE) -C '$(1)' -j 1 install
 endef
 
